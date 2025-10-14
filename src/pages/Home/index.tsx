7a8d6c24008b8ea973df6006e43c91fca6fac0ba
HebertPereira <hebertpktd@gmail.com>
2025-10-14 14:50:56 -0300

    chore: adding redict button to test on netlify

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import * as HomeStyles from "./styles.ts";
 
 function Home() {
+  const navigate = useNavigate();
   return (
     <HomeStyles.Container>
       My new App
       <>
         <></>
+        <button onClick={() => navigate("/register")}>Register</button>
       </>
     </HomeStyles.Container>
   );
